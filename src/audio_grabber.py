59b7153fe69eea874bcbaf548ade48b339c5d84f
AdrianTheHacker <adriantarantino2006@gmail.com>
2022-09-14 20:34:49 -0400

    Implementing youtube video download system

--- src/audio_grabber.py
+++ src/audio_grabber.py
@@ -2,6 +2,11 @@ import youtube_dl
 
 
 def download_song(song_url, song_name):
+    # Downloads a audio from youtube video, 
+    # and saves it as a mp3 inside of the audio library
+    #
+    # Credits to: https://dev.to/stokry/download-youtube-video-to-mp3-with-python-26p
+    
     video_info = youtube_dl.YoutubeDL().extract_info(
         url=song_url, 
         download=False
